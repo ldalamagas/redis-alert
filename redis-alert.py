161f3c53df49dbeec58d559cd78b3ef271bb6880
Lefteris Dalamagas <lefteris.dalamagas@gmail.com>
2013-10-10 17:04:46 +0300

    Initial Import

--- redis-alert.py
+++ redis-alert.py
@@ -2,9 +2,12 @@ import ConfigParser
 import argparse
 import email
 import logging
+from logging import StreamHandler
+from logging.handlers import SysLogHandler
 import smtplib
 import redis
 import socket
+from email.mime.text import MIMEText
 
 __author__ = 'ldalamagas'
 
@@ -14,8 +17,8 @@ config = {}
 logger = logging.getLogger("backup")
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("REDIS-ALERT[%(process)d] %(levelname)s: %(message)s")
-syslog_handler = logging.handlers.SysLogHandler(address="/dev/log")
-stream_handler = logging.StreamHandler()
+syslog_handler = SysLogHandler(address="/dev/log")
+stream_handler = StreamHandler()
 syslog_handler.setFormatter(fmt=formatter)
 logger.addHandler(syslog_handler)
 logger.addHandler(stream_handler)
@@ -24,36 +27,17 @@ logger.addHandler(stream_handler)
 def arguments():
     parser = argparse.ArgumentParser(description="Redis Alert")
     parser.add_argument("--config", default="redis-alert.cfg", help="Path to the configuration file")
-    parser.add_argument("--host", default="localhost")
-    parser.add_argument("--port", type=int, default=6379)
     return parser.parse_args()
 
 
-def read_config(configuration_file, config):
+def read_config(configuration_file):
+    global config
     logger.info("running redis-alert with [%s] configuration", configuration_file)
     cp = ConfigParser.ConfigParser()
     cp.readfp(open(configuration_file))
-
-    # General backup configuration
-    config["backup_items"] = (cp.get("backup", "items")).split(",")
-    config["backup_prefix"] = cp.get("backup", "prefix")
-    config["backup_suffix"] = cp.get("backup", "suffix")
-    config["retention_enabled"] = cp.getboolean("backup", "retention_enabled")
-    config["retention_period"] = cp.getint("backup", "retention")
-    config["tmp_dir"] = cp.get("backup", "temp_storage")
-
-    # MySQL
-    config["db_enabled"] = cp.getboolean("mysql", "enabled")
-    config["db_names"] = (cp.get("mysql", "names")).split(",")
-    config["db_host"] = cp.get("mysql", "host")
-    config["db_user"] = cp.get("mysql", "user")
-    config["db_password"] = cp.get("mysql", "password")
-
-    # Remote Storage
-    config["ftp_host"] = cp.get("ftp", "host")
-    config["ftp_dir"] = cp.get("ftp", "dir")
-    config["ftp_user"] = cp.get("ftp", "user")
-    config["ftp_password"] = cp.get("ftp", "password")
+    config["redis_host"] = cp.get("redis", "host")
+    config["redis_port"] = cp.getint("redis", "port")
+    config["redis_threshold"] = cp.getfloat("redis", "threshold")
 
     # Mail Notifications
     config["smtp_enabled"] = cp.getboolean("smtp", "enabled")
@@ -66,29 +50,45 @@ def read_config(configuration_file, config):
 
 def send_mail(message):
     smtp = None
-    try:
-        msg = email.MIMEText(message)
-        msg['Subject'] = 'Error while backing up [%s]' % socket.gethostname()
-        msg['From'] = config["smtp_from_address"]
-        msg['Reply-To'] = config["smtp_from_address"]
-        msg['To'] = config["smtp_to_address"]
+    msg = MIMEText(message)
+    msg['Subject'] = 'Redis Memory Threshold exceeded on [%s]' % socket.gethostname()
+    msg['From'] = config["smtp_from_address"]
+    msg['Reply-To'] = config["smtp_from_address"]
+    msg['To'] = ",".join(config["smtp_to_addresses"])
 
-        smtp = smtplib.SMTP(config["smtp_server"])
-        smtp.starttls()
-        smtp.login(config["smtp_user"], config["smtp_password"])
-        smtp.sendmail(config["smtp_from_address"], config["smtp_to_address"], msg.as_string())
-    except email.errors.MessageError:
-        logger.error("Error trying to notify recipients")
-    except email.SMTPAuthenticationError:
-        logger.error("Error trying to notify recipients, please check your smtp credentials")
-    finally:
-        smtp.quit()
+    smtp = smtplib.SMTP(config["smtp_server"])
+    smtp.starttls()
+    smtp.login(config["smtp_user"], config["smtp_password"])
+    smtp.sendmail(config["smtp_from_address"], config["smtp_to_addresses"], msg.as_string())
+    smtp.quit()
+    # try:
+    #     msg = email.MIMEText(message)
+    #     msg['Subject'] = 'Redis Memory Threshold exceeded on [%s]' % socket.gethostname()
+    #     msg['From'] = config["smtp_from_address"]
+    #     msg['Reply-To'] = config["smtp_from_address"]
+    #     msg['To'] = config["smtp_to_address"]
+    #
+    #     smtp = smtplib.SMTP(config["smtp_server"])
+    #     smtp.starttls()
+    #     smtp.login(config["smtp_user"], config["smtp_password"])
+    #     smtp.sendmail(config["smtp_from_address"], config["smtp_to_address"], msg.as_string())
+    # except email.errors.MessageError:
+    #     logger.error("Error trying to notify recipients")
+    # except email.SMTPAuthenticationError:
+    #     logger.error("Error trying to notify recipients, please check your smtp credentials")
+    # finally:
+    #     smtp.quit()
 
 if __name__ == '__main__':
     arguments = arguments()
-    r = redis.Redis(host=arguments.host, port=arguments.port)
+    read_config(arguments.config)
+    r = redis.Redis(host=config["redis_host"], port=config["redis_port"])
     um = 0
 
     used_memory = r.info()['used_memory']   # In Bytes
     used_memory_in_mb = used_memory/1024.0/1024.0
-    print "{0:.2f}".format(used_memory_in_mb)
+    if used_memory_in_mb > config["redis_threshold"]:
+        message = "Redis server memory consumpsion has reached to " \
+                  "[%.2f]MB, the configured threshold is [%.2f]MB" % (used_memory_in_mb, config["redis_threshold"])
+        logger.error(message)
+        send_mail(message)
